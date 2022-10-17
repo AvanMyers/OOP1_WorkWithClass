a6e46de250ebb659a20fb077744fd79a57bec59e
AvanMyers <smail1341@gmail.com>
2022-10-17 20:49:03 +0300

    job

---
+++ OOP_WorkWithClass/OOP_WorkWithClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_WorkWithClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(100,20,10);
+
+            player.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public int Health;
+        public int Damage;
+        public int Armor;
+
+        public Player(int health, int damage, int armot)
+        {
+            Health = health;
+            Damage = damage;
+            Armor = armot;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Здоровье - {Health}, Урон - {Damage}, Броня - {Armor}");
+        }
+    }
+}
---
+++ OOP_WorkWithClass/OOP_WorkWithClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("OOP_WorkWithClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOP_WorkWithClass")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0ed40eac-9e6f-4002-b08b-92f4db540c7b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
